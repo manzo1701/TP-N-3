418eb7ed5ccf4e522431bf3b9e77e30b0736d112
unknown <lautaroordenes.epet20@gmail.com>
2022-04-20 12:30:08 -0300

    finalizacion de la pagina

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var titulo = document.getElementById('titulaso');
+
+var til = document.createElement('h1');
+til.innerHTML = "EL RE TITULO"
+
+titulo.appendChild(til)
+//----------------------------------------------------
+var texto = document.getElementById('texto');
+
+var txt = document.createElement('p');
+txt.innerHTML = "esto es un texto el cual es parte de una pagina que termina siendo para un trabajo practico"
+
+texto.appendChild(txt)
